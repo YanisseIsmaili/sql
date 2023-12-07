9d6c76ba969ab20202d5a4642c52aee595c0c865
YanisseIsmaili <120456673+YanisseIsmaili@users.noreply.github.com>
2023-12-07 17:47:03 +0100

    create-table

--- create-table.sql
+++ create-table.sql
@@ -1,6 +1,7 @@
-{
-  name: 'StudentId',
-  type: 'INTEGER AUTOINCREMENT',
-  notnull: 1,
-  pk: 1
-}
+CREATE TABLE students (
+    StudentId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    FirstName VARCHAR(80) NOT NULL,
+    LastName VARCHAR(80) NOT NULL,
+    Birthday DATE NOT NULL,
+    City VARCHAR(50) NOT NULL
+);
