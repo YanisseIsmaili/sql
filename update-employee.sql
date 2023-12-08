e7920c2a9a29c98e0dab8ca3d4bf5825d6008e70
YanisseIsmaili <120456673+YanisseIsmaili@users.noreply.github.com>
2023-12-08 15:57:15 +0100

    update-employee

--- update-employee.sql
+++ update-employee.sql
@@ -1,3 +1,4 @@
 UPDATE employees
-SET Title = 'IT Manager', ReportsTo = (SELECT EmployeeId FROM employees WHERE LastName = 'Andrew' AND FirstName = 'Adams')
+SET Title = 'IT Manager', ReportsTo = (SELECT EmployeeId FROM employees WHERE LastName = 'Adams' AND FirstName = 'Andrew')
 WHERE LastName = 'King' AND FirstName = 'Robert';
+
