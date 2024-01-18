23115053058615fd4152a87fe6fbec4769cb5acc
iyanisse <yanisse.ismail@ynov.com>
2024-01-18 17:33:09 +0100

    zebi10

--- playlist-contains-song-selled.sql
+++ playlist-contains-song-selled.sql
@@ -2,3 +2,4 @@ SELECT p.PlaylistId, p.Name,
   IFNULL(ROUND((COUNT(pt.TrackId)* 100*1.0 / (SELECT COUNT(*) FROM playlist_track WHERE PlaylistId = p.PlaylistId)),4),0) AS '% song selled twice'
 FROM playlists p
 EFT JOIN playlist_track pt ON p.PlaylistId = pt.PlaylistId
+AND IFNULL(pt.TrackId,2) IN (
