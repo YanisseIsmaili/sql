c4d190630c9608b37af3d20a7039fdf966e5d52e
iyanisse <yanisse.ismail@ynov.com>
2024-01-18 17:43:16 +0100

    zebi10

--- stats-seller.sql
+++ stats-seller.sql
@@ -6,6 +6,7 @@ SELECT e.LastName, e.FirstName,
 	INNER JOIN customers c ON c.CustomerId = i.CustomerId 
 	WHERE c.SupportRepId = e.EmployeeId) AS 'Total Sell By Employee',
 	ifnull((SELECT BillingCountry FROM invoices i
+    
 	INNER JOIN customers c ON c.CustomerId = i.CustomerId
 	WHERE c.SupportRepId = e.EmployeeId
 	GROUP BY BillingCountry ORDER BY count(*) DESC LIMIT 1),'-') AS 'Country With Most Sales',
