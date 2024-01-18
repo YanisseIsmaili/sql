9c5d81bc9346138155766fd97dc68d74ef684176
iyanisse <yanisse.ismail@ynov.com>
2024-01-18 17:42:52 +0100

    zebi10

--- stats-seller.sql
+++ stats-seller.sql
@@ -1,1 +1,8 @@
 SELECT e.LastName, e.FirstName,
+
+(SELECT count(*) FROM invoices) AS 'Total Sell',
+ 
+(SELECT count(*) FROM invoices i
+	INNER JOIN customers c ON c.CustomerId = i.CustomerId 
+	WHERE c.SupportRepId = e.EmployeeId) AS 'Total Sell By Employee',
+	
