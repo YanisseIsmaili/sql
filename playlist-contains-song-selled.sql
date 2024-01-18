55c961618502a59cca782bb19a846f9bfcdff56b
iyanisse <yanisse.ismail@ynov.com>
2024-01-18 17:32:59 +0100

    zebi10

--- playlist-contains-song-selled.sql
+++ playlist-contains-song-selled.sql
@@ -1,2 +1,3 @@
 SELECT p.PlaylistId, p.Name,
   IFNULL(ROUND((COUNT(pt.TrackId)* 100*1.0 / (SELECT COUNT(*) FROM playlist_track WHERE PlaylistId = p.PlaylistId)),4),0) AS '% song selled twice'
+FROM playlists p
