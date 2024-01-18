f5a3da2560151e9e37f466052a58a4d4e4ed8fad
iyanisse <yanisse.ismail@ynov.com>
2024-01-18 17:43:46 +0100

    zebi10

--- stats-seller.sql
+++ stats-seller.sql
@@ -10,5 +10,12 @@ SELECT e.LastName, e.FirstName,
 	INNER JOIN customers c ON c.CustomerId = i.CustomerId
 	WHERE c.SupportRepId = e.EmployeeId
 	GROUP BY BillingCountry ORDER BY count(*) DESC LIMIT 1),'-') AS 'Country With Most Sales',
-    
+    ifnull((SELECT g.Name FROM genres g
+	INNER JOIN tracks t ON t.GenreId = g.GenreId
+	INNER JOIN invoice_items inv ON inv.TrackId = t.TrackId
+	INNER JOIN invoices i ON i.InvoiceId = inv.InvoiceId
+	INNER JOIN customers c ON c.CustomerId = i.CustomerId
+	WHERE c.SupportRepId = e.EmployeeId
+	GROUP BY g.Name ORDER BY count(*) DESC LIMIT 1),'-') AS 'Most Genre Selled',
+
 	
